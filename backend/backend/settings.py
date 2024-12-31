cc37c75e7a7810ca573b969986d5756d0e1dcc3b
caitlynfowkes <caitlynfowkes@gmail.com>
2024-12-31 14:00:47 +0200

    build: connect to postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,9 +88,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "PASSWORD": os.environ.get("DB_SECRET"),
+        "USER": os.environ.get("DB_USER"),
+        "PORT": "5432",
+        "HOST": "",  # "" defaults to localhost
     }
 }
 
