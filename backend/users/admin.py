268a7e75950d12b61e64445b2fe85b55d9117df8
caitlynfowkes <caitlynfowkes@gmail.com>
2025-01-04 18:15:49 +0200

    chore: set up admin for user

---
+++ backend/users/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class ExtendedUserAdmin(UserAdmin):
+    list_display = ["first_name", "last_name", "email", "status", "is_superuser"]
+    ordering = ["date_joined"]
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["email", "password"],
+            },
+        ),
+        (
+            "Personal Info",
+            {
+                "fields": ["first_name", "last_name"],
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": [
+                    "status",
+                    "is_active",
+                    "is_superuser",
+                    "is_staff",
+                    "groups",
+                    "user_permissions",
+                ],
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ["last_login", "date_joined", "deactivated_at"],
+            },
+        ),
+    ]
+
+
+admin.site.register(User, ExtendedUserAdmin)
