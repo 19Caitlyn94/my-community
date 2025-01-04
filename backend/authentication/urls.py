2a07ed67219ed15a5910287f144aa3194b647153
Caitlyn <caitlyn@gradconnection.com>
2025-01-04 18:27:36 +0200

    extend user model (#4)
    * feat: extend user model
    
    * chore: cleanup and formatting
    
    * chore: set up admin for user
    
    ---------
    
    Co-authored-by: caitlynfowkes <caitlynfowkes@gmail.com>

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,7 +3,6 @@ from . import views
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
-from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
 
