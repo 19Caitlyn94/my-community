6de53b4c15e612df55574d3ae8f2264188ee2bc8
caitlynfowkes <caitlynfowkes@gmail.com>
2025-01-07 19:02:24 +0200

    chore: add post and posttype to admin

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import PostType, Post
 
-# Register your models here.
+
+class PostTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "status", "posttype", "user"]
+    readonly_fields = ["deleted_at", "created_at", "updated_at"]
+
+
+admin.site.register(PostType, PostTypeAdmin)
+admin.site.register(Post, PostAdmin)
