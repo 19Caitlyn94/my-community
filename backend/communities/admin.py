d803ba925ae38cfed3dd6004b7f57d215e7735ec
caitlynfowkes <caitlynfowkes@gmail.com>
2025-01-07 14:49:09 +0200

    feat: add communities to admin

--- backend/communities/admin.py
+++ backend/communities/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Community
+
+
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created_by",
+        "created_at",
+        "updated_at",
+        "is_active",
+    ]
+    ordering = ["updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
