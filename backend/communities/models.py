45a51a3f07c44f7d9e4b556d43472cd40ff6b4f0
caitlynfowkes <caitlynfowkes@gmail.com>
2025-01-24 15:31:41 +0200

    feat: add community users join table and link it up with m2m field
    *with migrations

--- backend/communities/models.py
+++ backend/communities/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from utils.models import TimeStampedModel
 from django_extensions.db.fields import AutoSlugField, RandomCharField
 from django.contrib.auth import get_user_model
+from django.db.models.constraints import UniqueConstraint
 
 User = get_user_model()
 
@@ -16,6 +17,7 @@ class Community(TimeStampedModel, models.Model):
     created_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="created_communities"
     )
+    users = models.ManyToManyField(User, through="CommunityUser")
 
     class Meta:
         verbose_name = "Community"
@@ -30,3 +32,15 @@ class CommunityUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Community member"
+        verbose_name_plural = "Community members"
+
+        # Avoid duplicate entries
+        constraints = [
+            UniqueConstraint(fields=["community", "user"], name="unique_community_user")
+        ]
+
+    def __str__(self):
+        return f"{self.user.email} in {self.community.name}"
