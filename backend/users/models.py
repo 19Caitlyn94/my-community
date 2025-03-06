c7e2f62a2b2bcc37ee439b7b31085897cb383c7f
caitlynfowkes <caitlynfowkes@gmail.com>
2025-03-06 22:04:09 +0200

    chore: deprecate username field

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -10,9 +33,33 @@ class User(AbstractUser):
         ("deactivated", "Deactivated"),
     ]
 
+    username = None  # Remove username field
     email = models.EmailField(
         unique=True,
         error_messages={"unique": "A user with that email already exists."},
     )
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     deactivated_at = models.DateTimeField(null=True, blank=True)
+
+    profile_image = models.ImageField(
+        upload_to="profile_images/", blank=True, null=True
+    )
+    bio = models.TextField(max_length=255, blank=True, default="")
+
+    USERNAME_FIELD = "email"  # Use email as the username field
+    REQUIRED_FIELDS = []  # No additional required fields
+
+    objects = UserManager()  # Use our custom manager
+
+    @property
+    def display_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def profile_image_url(self):
+        if self.profile_image:
+            return f"{settings.BASE_HOST}/media/{self.profile_image}"
+        return ""
+
+    def __str__(self):
+        return self.email
