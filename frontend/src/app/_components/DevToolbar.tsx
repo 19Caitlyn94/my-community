da54eabc67f791670f27038559ae22e127af6654
caitlynfowkes <caitlynfowkes@gmail.com>
2025-03-09 20:17:28 +0200

    fix: devtoolbar width blocking login button

--- frontend/src/app/_components/DevToolbar.tsx
+++ frontend/src/app/_components/DevToolbar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function DevToolbar({}: Props) {
   return (
-    <div className="fixed bottom-0 right-0 left-0">
+    <div className="fixed bottom-0 left-0">
       <label className="swap swap-rotate">
         {/* this hidden checkbox controls the state */}
         <input type="checkbox" className="theme-controller" value="light" />
