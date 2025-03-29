82e6ccb0e31b6206e1de59d6926a9f60b30d2a0f
caitlynfowkes <caitlynfowkes@gmail.com>
2025-03-29 16:46:29 +0200

    feat: add cookies for last community viewed
    Takes user back to their last viewed community when they re-login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { auth } from "@/auth";
 
 import LandingPage from "./_pages/LandingPage";
-import Newsfeed from "./_pages/Newsfeed";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function Home() {
   const session = await auth();
   const isLoggedIn = session?.user;
+  const firstCommunity = session?.user?.communities[0].id;
+  const cookieStore = await cookies();
+  const lastViewedCommunity: string | undefined = cookieStore.get(
+    "lastViewedCommunity"
+  )?.value;
 
   if (isLoggedIn) {
-    return <Newsfeed />;
+    redirect(`/${lastViewedCommunity || firstCommunity}`);
   }
 
   return <LandingPage />;
