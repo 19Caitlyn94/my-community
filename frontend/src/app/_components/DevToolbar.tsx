45c5985b844ee4b43a398568660d2898f46ec05d
caitlynfowkes <caitlynfowkes@gmail.com>
2025-04-28 10:47:03 +0200

    style: adjust position of the devtoolbar

--- frontend/src/app/_components/DevToolbar.tsx
+++ frontend/src/app/_components/DevToolbar.tsx
@@ -3,17 +3,17 @@ import { Icon, ICONS } from "@/app/_components";
 
 function DevToolbar() {
   return (
-    <div className="fixed bottom-0 left-0">
+    <div className="fixed bottom-14 left-5">
       <label className="swap swap-rotate">
         {/* this hidden checkbox controls the state */}
         <input type="checkbox" className="theme-controller" value="light" />
         <Icon
-          className="swap-on w-10 h-10"
+          className="swap-on w-8 h-10"
           iconType={ICONS.moon}
           title="Light theme"
         />
         <Icon
-          className="swap-off w-10 h-10"
+          className="swap-off w-8 h-10"
           iconType={ICONS.sun}
           title="Dark theme"
         />
